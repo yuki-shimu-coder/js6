1384e1b5367bf4843ea2e648d8f3e2eaaf56f9b2
yuki-shimu-coder <68466008+yuki-shimu-coder@users.noreply.github.com>
2021-09-29 21:17:57 +0900

    配列の分割代入 配列の分割代入

--- src/index.js
+++ src/index.js
@@ -116,3 +116,7 @@
 const myProfile = ["yuki", 28];
 const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です`;
 console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です`;
+console.log("message4:" + message4);
